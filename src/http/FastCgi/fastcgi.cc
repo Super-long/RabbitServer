5d27a67dd8520cefce0a289d33bd5460a2d12ac7
Super-long <2339824768@qq.com>
2020-02-21 22:38:28 +0800

    fc

---
+++ src/http/FastCgi/fastcgi.cc
@@ -0,0 +1,216 @@
+#include "fastcgi.h"
+#include <netinet/in.h> //sockaddr_in
+#include <arpa/inet.h> //inet_addr
+//#include <sys/types.h>
+#include <sys/socket.h> //connect
+#include <iostream> //cout endl
+#include <assert.h> //assert
+
+namespace ws{
+
+void 
+FastCgi::Conection(){
+    struct sockaddr_in ServerAddress;
+    memset(&ServerAddress, 0, sizeof ServerAddress);
+    constexpr char IP[] = "127.0.0.1";
+    
+    ServerAddress.sin_family = AF_INET;
+    ServerAddress.sin_addr.s_addr = inet_addr(IP);
+    ServerAddress.sin_port = htons(9000); 
+
+    int ret = ::connect(socket_.fd(), (struct sockaddr *)&ServerAddress, sizeof ServerAddress);
+    if(ret < 0) {
+        std::cout << __FILE__ << "      " << __LINE__ << std::endl ;
+    }
+    assert(ret >= 0);
+}
+
+FCGI_Header 
+CreateHeader(int type, int request, int contentLength, int paddingLength){
+    FCGI_Header header;
+    header.version = FCGI_VERSION_1; //版本一般设置为1
+    header.type    = (unsigned char)type; //设置请求类型
+        
+    //用两个字段保存请求ID
+    header.requestIdB1 = (unsigned char)((request >> 8) & 0xff); //高八位
+    header.requestIdB0 = (unsigned char)(request & 0xff); //第八位
+
+    //用两个字段保存内容长度
+    header.contentLengthB1 = (unsigned char)((contentLength >> 8) & 0xff); //高八位
+    header.contentLengthB0 = (unsigned char)(contentLength & 0xff); //低八位
+    
+    header.paddingLength = (unsigned char)paddingLength; //填充字节的长度 保证字节对齐 TODO 查下
+    header.reserved = 0; //保留字节赋为0  
+    return header; //RVO
+}
+
+FCGI_BeginRequestBody
+FastCgi::CreateBeginRequestBody(int role,int keepConnection){
+    FCGI_BeginRequestBody body;
+
+    body.roleB1 = (unsigned char)((role >> 8) & 0xff);//两个字节表示期望php-fpm扮演的角色 一般为响应者
+    body.roleB0 = (unsigned char)(role & 0xff);
+
+    body.flags = (unsigned char)((keepConnection) ? FCGI_KEEP_CONN : 0);//大于0常连接，否则短连接
+
+    bzero(&body.reserved,sizeof(body.reserved));
+
+    return body;
+}
+
+bool
+FastCgi::StartRequest(){
+    FCGI_BeginRequestRecord FirstRecord;
+
+    FirstRecord.header = CreateHeader(FCGI_BEGIN_REQUEST, requestId_, sizeof FirstRecord.body);
+    FirstRecord.body = CreateBeginRequestBody(FCGI_RESPONDER, 0); //当做响应器 发出消息执行后返回
+
+    int ret = ::write(socket_.fd(), (char *)&FirstRecord, sizeof FirstRecord);
+    assert(ret == sizeof(FirstRecord));
+    return true;
+}
+
+void
+FastCgi::CreateContentValue(const std::string& name, int nameLen, //ContentBuffPtr为要写入的地方
+    const std::string& value, int valueLen, unsigned char* ContentBuffPtr, int* ContentLen){
+    
+    unsigned char* StartSpot_Ptr = ContentBuffPtr;
+
+    if(nameLen < 128){ //如果nameLen长度小于128字节
+        *ContentBuffPtr++ = (unsigned char)nameLen; //nameLen用一个字节保存
+    }else{ //nameLen用4个字节保存
+        *ContentBuffPtr++ = (unsigned char)((nameLen >> 24) | 0x80); //感觉后面并没有什么意义
+        *ContentBuffPtr++ = (unsigned char)(nameLen >> 16); //自动去除高位
+        *ContentBuffPtr++ = (unsigned char)(nameLen >> 8);
+        *ContentBuffPtr++ = (unsigned char)nameLen;
+    }
+    if(valueLen < 128){
+        *ContentBuffPtr++ = (unsigned char)valueLen;
+    }else{
+        *ContentBuffPtr++ = (unsigned char)((valueLen >> 24) | 0x80);
+        *ContentBuffPtr++ = (unsigned char)(valueLen >> 16);
+        *ContentBuffPtr++ = (unsigned char)(valueLen >> 8);
+        *ContentBuffPtr++ = (unsigned char)valueLen;
+    }
+    std::for_each(name.cbegin(), name.cend(), [&](char ch){*ContentBuffPtr++ = ch;});
+    std::for_each(value.cbegin(), value.cend(), [&](char ch){*ContentBuffPtr++ = ch;});
+
+    *ContentLen = std::distance(StartSpot_Ptr, ContentBuffPtr);
+}
+
+void 
+FastCgi::SendContent(const std::string& tag, const std::string& value){
+    unsigned char bodyBuffer[CONTENT_BUFFER_LEN];
+    memset(bodyBuffer, 0 , sizeof bodyBuffer);
+
+    int ContentLength = 0; 
+    CreateContentValue(tag, tag.size(), value, value.size(), bodyBuffer, &ContentLength); //得到发送的内容与长度
+    
+    FCGI_Header ContentHeader(CreateHeader(FCGI_PARAMS, requestId_, ContentLength));
+
+    int AllContentLength = ContentLength + FCGI_HEADER_LEN;
+    char Content[AllContentLength];
+    memset(Content, 0, AllContentLength*sizeof(char));
+
+    memcpy(Content, (char*)&ContentHeader, FCGI_HEADER_LEN);
+    memcpy(Content + FCGI_HEADER_LEN, Content, ContentLength);
+
+    int ret = ::write(socket_.fd(), Content, AllContentLength);
+    assert(ret == AllContentLength);
+}
+
+bool 
+FastCgi::EndRequest(){
+    FCGI_Header EndHeader(CreateHeader(FCGI_PARAMS, requestId_, 0));
+
+    int ret = ::write(socket_.fd(), (char *)&EndHeader, FCGI_HEADER_LEN);
+    if(ret < 0) return false;
+    assert(ret == FCGI_HEADER_LEN);
+
+    return true;
+}
+
+void
+FastCgi::SendRequest(const std::string& data, size_t len){
+    FCGI_Header IntervalHead(CreateHeader(FCGI_STDIN, requestId_, len)); //发送的输入
+    int ret_IntervalHead = ::send(socket_.fd(), (char*)&IntervalHead, FCGI_HEADER_LEN, 0);
+    assert(ret_IntervalHead == FCGI_HEADER_LEN);
+    
+    int ret_Data = ::send(socket_.fd(), data.c_str(), len, 0);
+    assert(ret_Data == len);
+
+    FCGI_Header EndHeader(CreateHeader(FCGI_STDIN, requestId_, 0));
+    int ret_EndHead = ::send(socket_.fd(), (char*)&EndHeader, FCGI_HEADER_LEN, 0);
+    assert(ret_EndHead == FCGI_HEADER_LEN);
+    //网络上的问题目前不太好解决, 消息发送不完全的话就退出
+}
+
+std::string
+FastCgi::ReadContent(){
+    FCGI_Header parsering;
+    char Content[CONTENT_BUFFER_LEN];
+    memset(Content, 0, sizeof Content);
+    int ContentLength = 0;
+    int ret = 0;
+    char Trash[128];
+    memset(Trash, 0 , sizeof Trash);
+
+    while(::read(socket_.fd(), &parsering, FCGI_HEADER_LEN) > 0){
+        if(parsering.type == FCGI_STDOUT){ //正常输出
+            ContentLength = (parsering.contentLengthB1 << 8) + (parsering.contentLengthB0);//得到内容长度
+            memset(Content, 0, CONTENT_BUFFER_LEN);
+            //读取获取的内容
+            ret = ::read(socket_.fd(), Content, ContentLength); //得到内容
+            assert(ret == ContentLength); 
+
+            GetContent(Content); //Content中为HTML格式
+
+            if(parsering.paddingLength > 0){
+                ret = read(socket_.fd(), Trash, parsering.paddingLength);
+                assert(ret == parsering.paddingLength);
+            }
+        }else if(parsering.type == FCGI_STDERR){ //错误输出
+            ContentLength = (parsering.contentLengthB1 << 8) + (parsering.contentLengthB0);
+            memset(Content, 0, CONTENT_BUFFER_LEN);
+            
+            ret = ::read(socket_.fd(), Content, ContentLength); //得到内容
+            assert(ret == ContentLength); 
+
+            if(parsering.paddingLength > 0){
+                ret = read(socket_.fd(), Trash, parsering.paddingLength);
+                assert(ret == parsering.paddingLength);
+            }    
+        }else if(parsering.type == FCGI_STDERR){ //错误输出
+            FCGI_EndRequestBody endRequest;
+
+            ret = read(socket_.fd(), &endRequest, sizeof endRequest);
+            assert(ret == sizeof(endRequest));
+            std::cout << "内容接收至结尾.\n";
+        }
+    }
+    return Content;
+}
+
+void
+FastCgi::GetContent(char* data){
+    char *pt;   //保存html内容开始位置
+
+    if(1 == HtmlFlag){   //读取到的content是html内容
+    //TODO 这个值得搞一手
+    }else{
+        if((pt = FindStart(data)) != NULL){
+            HtmlFlag = 1;
+            for(char* i = pt;*i != '\0';i++){}        
+        }          
+    }  
+}
+
+char* FastCgi::FindStart(char* data){
+    for(;*data != '\0';data++){
+        if(*data == '<')
+            return data;
+    }
+    return NULL;
+}
+
+}
--- src/http/FastCgi/fastcgi.h
+++ src/http/FastCgi/fastcgi.h
@@ -2,16 +2,35 @@
 #define FASTCGI_H_
 
 #include "fastcgiHeader.h"
+#include "../../net/socket.h"
 
 namespace ws{
 
 class FastCgi{
 private:
+int requestId_ = 0; //TODO 这个的分配很重要
+int HtmlFlag = 0;
+Socket socket_;
 
+static const int CONTENT_BUFFER_LEN = 1024;
 public:
     //生成头部
-    FCGI_Header makeHeader(int type,int request,
-                            int contentLength,int paddingLength);
+    void SetRequestID(int ID) noexcept {requestId_ = ID;}
+    
+    void Conection(); //连接php-fpm服务器
+    bool StartRequest(); //开始一次请求
+    bool EndRequest();
+    FCGI_Header CreateHeader(int type, int request, int contentLength, int paddingLength = 0);
+    FCGI_BeginRequestBody CreateBeginRequestBody(int role,int keepConnection);
+    void SendContent(const std::string& tag, const std::string& value); //设置消息体
+    //生成PARAMS的name-value body
+    void CreateContentValue(const std::string& name,int nameLen,
+                            const std::string& value,int valueLen,
+                            unsigned char* ContentBuffPtr, int* ContentLen);
+    void SendRequest(const std::string& data, size_t len);
+    std::string ReadContent(); //从套接字读取消息
+    void GetContent(char* data); //还要再改
+    char* FindStart(char* data);
 };
 
 }
